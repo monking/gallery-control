909a9c54f005f7e8a49bf99d4ff91c92cb46d963
Christopher Lovejoy <c@lovejoy.io>
2024-02-15 17:25:11 -0800

    Improved pointer interaction, new `unset` method
    Changed behavior in 0.1.5 from 0.1.4:
    - Must explicitly unpause, or set option allowAutoStart: true. Will not begin playing after doing next/previous.
    - Tapping middle toggles pause
    - Other refinements around state
    
    - New global variable galleryControlInterface
    - New method `unset` reverses `init`, enabling changing versions without reload

--- README.md
+++ README.md
@@ -1,12 +1,46 @@
+# Gallery Control browser extension
+
+View next/previous image in a sequence with tap, click, or keyboard.
+
+## how to use
+
+### commands
+
+- **next** shows the image *after* the current one.  
+  (key: `Right Arrow`, pointer: Right)
+- **previous** shows the image *before* the current one.  
+  (key: `Left Arrow`, pointer: Left)
+- **larger** shows the *full size* version of the current image.  
+  (key: `Up Arrow`, pointer: Top)
+- **smaller** shows the *thumbnail* version of the current image.  
+  (key: `Down Arrow`, pointer: Bottom)
+- **pause**/**unpause** continues *next*/*previous* at the *same rate*\*.  
+  (key: `Space`, pointer: Middle)
+- **stop** pauses and resets rate.  
+  (key: `Escape`, pointer: N/A)
+
+\*The rate when unpaused is taken from the recent rate of issuing "next"/"previous" commands.
+It defaults to 1Hz if the user has not established a rate, or if that rate is slower than the minimum (currently 0.25Hz, or once every 4 seconds).
+
+### pointer regions
+
+Left, Right, Top, Middle, Bottom
+```
++-------+
+| | T | |
+| |---| |
+|L| M |R|
+| |---| |
+| | B | |
++-------+
+```
+- Left and Right are the outer 25% horizontally.
+- Top and Bottom are the outer 25% vertically (excluding after Left and Right)
+- Middle is the 50% left over in the middle.
 
 
-### To use on Android:
-1. Install Firefox Nightly for Android
-2. In *Settings* -> *About*, tap Firefox Nightly logo 5x, enabling debug mode
-3. Tap *Install add-on from file* in the *Advanced* section.
-4. Choose the `.xpi` file...
+## how to build
 
-### How to build the XPI file
+Run `npm start` to build the packaged extension, and also prepare an unpacked version with manifest version 3 (for loading into Chrome).
 
-Run `npm start`.
-This creates a ZIP file with the `.xpi` file extension in `dist/` directory.
+Run `npm run` to list other script commands.
--- src/gallery-control.js
+++ src/gallery-control.js
@@ -1,20 +1,27 @@
-((options = {}) => {
+if (typeof window?.galleryControlInterface?.unset === 'function') {
+  window.galleryControlInterface?.unset();
+}
+window.galleryControlInterface = ((options = {}) => {
   const {
     imageSelector,
-		thumbPattern,
-		fullPattern,
-		fullReplacetoThumb,
+    thumbPattern,
+    fullPattern,
+    fullReplacetoThumb,
     waitForError,
-		cursorTimeout,
+    cursorTimeout,
     allowAutoAdvance,
+    allowAutoStart,
+    isDebugging,
   } = {
     imageSelector: 'img',
-		thumbPattern: /^(.*\/)(thumb_)([^\/]+.jpg)/,
-		fullPattern: /^(.*\/)([^\/]+.jpg)/,
-		fullReplacetoThumb: '$1thumb_$2',
+    thumbPattern: /^(.*\/)(thumb_)([^\/]+.jpg)/,
+    fullPattern: /^(.*\/)([^\/]+.jpg)/,
+    fullReplacetoThumb: '$1thumb_$2',
     waitForError: 1000,
-		cursorTimeout: 1000,
+    cursorTimeout: 1000,
     allowAutoAdvance: true,
+    allowAutoStart: false,
+    isDebugging: false,
     ...options,
   };
   const getImage = () => document.querySelector(imageSelector);
@@ -37,82 +44,127 @@
     }
     return url;
   };
-	const changeImage = (deltas, options = {}) => {
-		if (errorCooldownTimeout) {
+  const changeImage = (delta, options = {}) => {
+    if (errorCooldownTimeout) {
       console.info('The image failed to load recently. Waiting to let the user and/or server cool down.');
       return;
-		}
-		const {
-			whichImage,
-			withIntent,
-		} = {
-			withIntent: false,
-			...options,
-		};
-		const img = whichImage || getImage();
-		const currentUrl = img.src;
-		let newUrl = currentUrl;
-		Object.keys(deltas).forEach((dimension) => {
-			const delta = deltas[dimension];
-			const sign = Math.sign(delta);
-			if (sign === 0) return;
-			switch(dimension) {
-				case 'position':
-					newUrl = advanceUrl(newUrl, delta);
-					if (withIntent && allowAutoAdvance) {
-						autoAdvance.bump(delta);
-					}
-					break;
-				case 'size':
-					newUrl = sign === 1 ? largerUrl(newUrl) : smallerUrl(newUrl);
-					if (withIntent) {
-						autoAdvance.pause();
-					}
-					break;
-			}
-		});
-		if (newUrl !== currentUrl) {
-			img.src = newUrl;
-		}
-		return img;
-	};
+    }
+    const {
+      whichImage,
+      withIntent,
+    } = {
+      withIntent: false,
+      ...options,
+    };
+    const img = whichImage || getImage();
+    const currentUrl = img.src;
+    let newUrl = currentUrl;
+    if (withIntent) {
+      autoAdvance.pause();
+      if (allowAutoAdvance && delta.position !== 0) {
+        autoAdvance.bump(delta.position);
+      }
+    }
+    Object.keys(delta).forEach((dimension) => {
+      const value = delta[dimension];
+      const sign = Math.sign(value);
+      if (sign === 0) return;
+      switch(dimension) {
+        case 'position':
+          newUrl = advanceUrl(newUrl, value);
+          break;
+        case 'size':
+          newUrl = sign === 1 ? largerUrl(newUrl) : smallerUrl(newUrl);
+          break;
+      }
+    });
+    if (newUrl !== currentUrl) {
+      img.src = newUrl;
+    }
+    return img;
+  };
   let errorCooldownTimeout = null;
   const handleImageError = () => {
     autoAdvance.stop();
     errorCooldownTimeout = setTimeout(() => {errorCooldownTimeout = null}, waitForError);
   };
-  const handleMouseDown = (e) => {
-    if (e.defaultPrevented) return;
-    if (e.button === 1) {
-			autoAdvance.togglePause();
-      return;
-    }
+  const getPointerPosition = (event) => {
+    return {
+      x: event.pageX ?? event.clientX,
+      y: event.pageY ?? event.clientY,
+      xMin: window.scrollX,
+      yMin: window.scrollY,
+      xSpan: document.body.offsetWidth || window.innerWidth,
+      ySpan: document.body.offsetHeight || window.innerHeight,
+      originalEvent: event,
+    };
+  };
+  const getDeltaFromPointerPosition = (inputPointerInfo = {}) => {
+    const {
+      x,
+      y,
+      xSpan,
+      ySpan,
+      xMin,
+      yMin,
+    } = pointerInfo = {
+      xMin: 0,
+      yMin: 0,
+      ...inputPointerInfo
+    };
     // Normalize from -1 to 1
-    const xNormalized = (e.clientX / window.innerWidth) * 2 - 1;
-    const yNormalized = (e.clientY / window.innerHeight) * 2 - 1;
-		const delta = {
-			position: 0,
-			size: 0,
-		};
+    const xNormalized = ((x-xMin) / xSpan) * 2 - 1;
+    const yNormalized = ((y-yMin) / ySpan) * 2 - 1;
+    const delta = {
+      position: 0,
+      size: 0,
+    };
     if (Math.abs(xNormalized) > 0.5) {
       delta.position = Math.sign(xNormalized);
     } else if (Math.abs(yNormalized) > 0.5) {
       delta.size = Math.sign(-1 * yNormalized);
-      // Invert vertical, so that top is negative.
+      // Invert vertical, so that bottom is negative.
     }
-		changeImage(delta, {withIntent: true});
+    if (isDebugging) console.log('computed delta from pointerInfo', delta, pointerInfo);
+    return delta;
   };
+  const isDeltaNonZero = (delta) => {
+    // Same as (but generic): // return Boolean(delta.position || delta.size);
+    return Object.keys(delta).reduce((acc, dimension) => acc || (delta[dimension] !== 0), false);
+  };
+  const handleMouseDown = (e) => {
+		// Let secondary/right click through.
+    if (e.defaultPrevented || e.button === 2 || e.altKey || e.ctrlKey) return;
+    e.preventDefault();
+    const _defaultPointerAction = (ignore) => {
+      if (allowAutoAdvance) {
+        autoAdvance.togglePause();
+      } else {
+        autoAdvance.stop();
+      }
+    }
+    if (e.button === 1) {
+      return _defaultPointerAction();
+    }
+    const delta = getDeltaFromPointerPosition(getPointerPosition(e));
+    if (isDeltaNonZero(delta)) {
+      changeImage(delta, {withIntent: true});
+    } else {
+      _defaultPointerAction(e);
+    }
+  };
+  const CLASS_HIDE_CURSOR = 'hide-cursor';
   let isHidingMouse = false;
   let hideMouseTimeout = null;
   const handleMouseMove = (e) => {
     if (e.defaultPrevented) return;
     if (isHidingMouse) {
-      document.body.classList.remove('hide-cursor');
+      document.body.classList.remove(CLASS_HIDE_CURSOR);
       isHidingMouse = false;
     }
     clearTimeout(hideMouseTimeout);
     hideMouseTimeout = setTimeout(() => {
-      document.body.classList.add('hide-cursor');
+      document.body.classList.add(CLASS_HIDE_CURSOR);
       isHidingMouse = true;
     }, cursorTimeout);
   };
@@ -122,88 +174,83 @@
       startRate: 1,
       minRate: 0.25,
       maxRate: 10,
-			minSpan: 3, // how many steps before auto takes on a rate
-			maxSpan: 5,
-			maxIntervalScale: 3,
+      minSpan: 2, // how many steps before auto takes on a rate
+      maxSpan: 4,
+      maxIntervalScale: 3,
       ...options,
     };
-		const {
-			startRate,
-			minRate,
-			maxRate,
-			maxSpan,
-		} = autoAdvanceOptions;
-		const state = {
-			rate: 0,
-			isPaused: true,
-			direction: 0,
-			autoAdvanceTimeout: null,
-			times: [],
-		};
-		const getAvgRateFromTimes = (times, options = {}) => {
-			const {
-				minSpan,
-				maxSpan,
-				maxIntervalScale,
-			} = options;
-			if (times.length < minSpan) return 0;
-			const span = maxSpan >= minSpan
-				? Math.min(maxSpan, times.length)
-				: times.length;
-			if (times[0] - times[1] > maxIntervalScale * (times[1] - times[2])) {
-				times.splice(1);
-				return 0;
-			}
-			const avgIntervalMs = (times[0] - times[span - 1]) / span;
-			return 1000/avgIntervalMs;
-		};
+    const {
+      startRate,
+      minRate,
+      maxRate,
+      maxSpan,
+    } = autoAdvanceOptions;
+    const state = {
+      rate: 0,
+      isPaused: true,
+      direction: 0,
+      autoAdvanceTimeout: null,
+      times: [],
+    };
+    const getAvgRateFromTimes = (times, options = {}) => {
+      const {
+        minSpan,
+        maxSpan,
+        maxIntervalScale,
+      } = options;
+      if (times.length < minSpan) return 0;
+      const span = maxSpan >= minSpan
+        ? Math.min(maxSpan, times.length)
+        : times.length;
+      if (times[0] - times[1] > maxIntervalScale * (times[1] - times[2])) {
+        times.splice(1);
+        return 0;
+      }
+      const avgIntervalMs = (times[0] - times[span - 1]) / span;
+      return 1000/avgIntervalMs;
+    };
     const clearAdvanceInterval = () => {
       clearTimeout(state.autoAdvanceTimeout);
       state.autoAdvanceTimeout = null;
     };
-		const bump = (influence = 0) => {
-			clearAdvanceInterval();
-      const influenceSign = Math.sign(influence);
-      if (influenceSign !== 0) {
-				if (influenceSign !== state.direction) {
-					state.times.splice(0);
-					if (state.direction === 0) {
-						state.direction = influenceSign;
-					} else {
-						state.direction = 0;
-					}
-				}
+    const bump = (influence = 0) => {
+      clearAdvanceInterval();
+      state.direction = Math.sign(influence);
+      state.times.unshift(Date.now());
+      while (state.times.length > maxSpan) {
+        state.times.pop();
       }
-      if (state.direction !== 0) {
-				state.times.unshift(Date.now());
-				while (state.times.length > maxSpan) {
-					state.times.pop();
-				}
-				const observedRate = getAvgRateFromTimes(state.times, autoAdvanceOptions);
-				if (observedRate >= minRate) {
-					state.rate = Math.min(observedRate, maxRate);
-					state.isPaused = false;
-					next();
-				} else {
-					state.rate = 0;
-				}
+      const observedRate = getAvgRateFromTimes(state.times, autoAdvanceOptions);
+      if (observedRate >= minRate) {
+        state.rate = Math.min(observedRate, maxRate);
+        if (allowAutoStart) {
+          state.isPaused = false;
+          next();
+        }
+      } else {
+        pause();
       }
-		};
+      if (isDebugging) console.log('bump auto', {influence, ...state});
+    };
     const next = () => {
-			if (!state.isPaused && state.direction !== 0) {
-				changeImage({position: state.direction, size: -1});
+      if (isDebugging) console.log('next auto', state);
+      if (!state.isPaused && state.direction !== 0) {
+        changeImage({position: state.direction, size: -1});
       }
       if (state.rate > 0) {
         const delay = 1000/state.rate;
         state.autoAdvanceTimeout = setTimeout(next, delay);
-			}
+      }
     };
     const start = () => {
-			if (state.rate === 0) {
-				state.rate = startRate;
-			}
+      if (state.rate === 0) {
+        state.rate = startRate;
+        if (state.direction === 0) {
+          state.direction = 1;
+        }
+      }
+      state.isPaused = false;
       next();
-			state.isPaused = false;
     };
     const pause = () => {
       state.isPaused = true;
@@ -212,6 +259,8 @@
     const stop = () => {
       pause();
       state.rate = 0;
+      state.direction = 0;
+      state.times = [];
     };
     const togglePause = () => {
       if (state.isPaused) {
@@ -228,10 +277,10 @@
     if (e.altKey || e.ctrlKey) {
       return; // No modifiers, currently
     }
-		const delta = {
-			position: 0,
-			size: 0,
-		};
+    const delta = {
+      position: 0,
+      size: 0,
+    };
     switch(e.code) {
       case "Escape":
         autoAdvance.stop();
@@ -251,41 +300,66 @@
       case "ArrowDown":
         delta.size = -1;
         break;
-			default:
-				return;
+      default:
+        return;
     }
-		e.preventDefault();
+    e.preventDefault();
     if (delta.position !== 0 || delta.size !== 0) {
-			changeImage(delta, {withIntent: true});
+      changeImage(delta, {withIntent: true});
     }
   };
+  const CLASS_GALLERY_HOOKED = 'with-gallery-control';
+  const CLASS_GALLERY_CREATED = 'by-gallery-control';
   const init = () => {
+    if (document.body.classList.contains(CLASS_GALLERY_HOOKED)) {
+      console.error(`Gallery Control is already initialized.`);
+      return false;
+    }
     const img = getImage();
     if (!img) {
       return;
     }
+    document.body.classList.add(CLASS_GALLERY_HOOKED);
     const style = document.createElement('style');
+    style.classList.add(CLASS_GALLERY_CREATED);
     style.innerHTML =
       'img {' +
-      '  object-fit: contain;' +
-      '  width: 100vw;' +
-      '  height: 100vh;' +
+				'object-fit: contain;' +
+				'width: 100vw;' +
+				'height: 100vh;' +
+				'transition-property: opacity;' +
+				'transition-duration: 2s;' +
       '}'+
       '.hide-cursor {' +
-      '  cursor: none;' +
-      '}';
+				'cursor: none;' +
+      '}' +
+			'';
       // WISH (B) 2024-02-08 Enable zooming in, but still fit thumbnail and full to window normally. Might not be a CSS solution.
     document.body.appendChild(style);
     img.addEventListener('error', handleImageError);
     window.addEventListener('mousedown', handleMouseDown);
     window.addEventListener('mousemove', handleMouseMove);
-    //window.addEventListener('wheel', handleWheel);
     window.addEventListener('keydown', handleKey);
     return {
-			getImage,
+      init,
+      unset,
+      getImage,
       changeImage,
       autoAdvance,
     };
-  }
+  };
+  const unset = () => {
+    // Undo init.
+    document.body.querySelectorAll(`.${CLASS_GALLERY_CREATED}`)
+      .forEach((element) => element.parentElement.removeChild(element));
+    const img = getImage();
+    if (img) {
+      img.removeEventListener('error', handleImageError);
+    }
+    window.removeEventListener('mousedown', handleMouseDown);
+    window.removeEventListener('mousemove', handleMouseMove);
+    window.removeEventListener('keydown', handleKey);
+    document.body.classList.remove(CLASS_GALLERY_HOOKED, CLASS_HIDE_CURSOR);
+  };
   return init();
-})({minSpan:2});
+})();
